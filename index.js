c427e716af67eaafa078ea1614ac353d2f059efc
DucHQ <tintin1812@gmail.com>
2017-08-17 18:00:22 +0700

    -2

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.static(__dirname + '/public'));
 // Chatroom
 
 var numUsers = 0;
+var lastPlayderID = 0;
 
 io.on('connection', function (socket) {
   var addedUser = false;
@@ -30,19 +31,36 @@ io.on('connection', function (socket) {
   // when the client emits 'add user', this listens and executes
   socket.on('add user', function (username) {
     if (addedUser) return;
-
+    //add id
+    socket.player = {
+        id: lastPlayderID++,
+        x: randomInt(100,400),
+        y: randomInt(100,400)
+    };
     // we store the username in the socket session for this client
     socket.username = username;
     ++numUsers;
     addedUser = true;
     socket.emit('login', {
-      numUsers: numUsers
+	  numUsers: numUsers,
+      yourid: socket.player.id,
+      players: getAllPlayers()
     });
     // echo globally (all clients) that a person has connected
     socket.broadcast.emit('user joined', {
       username: socket.username,
-      numUsers: numUsers
+      numUsers: numUsers,
+      player: socket.player
     });
+    console.log('add user: %s, current user: %d', socket.username, numUsers);
+    // check enought player
+    if( numUsers > 1)
+    {
+    	setTimeout(function() {
+    		if( numUsers > 1)
+    			enoughPlayer();
+		}, 1000);
+    }
   });
 
   // when the client emits 'typing', we broadcast it to others
@@ -71,4 +89,26 @@ io.on('connection', function (socket) {
       });
     }
   });
+
 });
+
+function getAllPlayers(){
+    var players = [];
+    Object.keys(io.sockets.connected).forEach(function(socketID){
+        var player = io.sockets.connected[socketID].player;
+        if(player) players.push(player);
+    });
+    return players;
+}
+
+function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+function enoughPlayer(){
+	console.log('game prepare: %s', Date.now());
+	io.emit('game prepare', {
+      time: (Date.now() + 10000),
+      players: getAllPlayers()
+    });
+}

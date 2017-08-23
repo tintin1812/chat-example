499084f4669980015773d9f4fc21ccbc37a4615f
DucHQ <tintin1812@gmail.com>
2017-08-23 10:59:35 +0700

    -uu

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(express.static(__dirname + '/public'));
 
 // Chatroom
 var lastPlayderID = 0;
+var countQuestion = 0;
+var maxQuestion = 10;
+var requestPlayerToStart = 1;
+
 var GameState = {
   WAIT_ENOUGHT_PLAYERS: 1,
   PREPARE_GAME: 2,
@@ -42,11 +46,13 @@ io.on('connection', function (socket) {
     socket.player = {
         id: lastPlayderID++,
         x: 0,
-        y: 0
+        y: 0,
+        z: 0
     };
     if(movedata) {
       socket.player.x = movedata.x;
-      socket.player.y = movedata.y; 
+      socket.player.y = movedata.y;
+      socket.player.z = movedata.z;
     }
     // we store the username in the socket session for this client
     socket.username = username;
@@ -63,9 +69,10 @@ io.on('connection', function (socket) {
       numUsers: gameData.numUsers,
       player: socket.player
     });
-    console.log('add user: %s, numUsers: %d, pos: %f, %f', socket.username, gameData.numUsers, socket.player.x, socket.player.y);
+    console.log('add user: %s, numUsers: %d, id: %d, pos: %f, %f, %f', socket.username, gameData.numUsers, socket.player.id, socket.player.x, socket.player.y, socket.player.z);
     // check enought player
-    if( gameData.numUsers > 1 && gameData.gameState == GameState.WAIT_ENOUGHT_PLAYERS)
+    if(((countQuestion == 0 && gameData.numUsers >= requestPlayerToStart) || countQuestion > 0) 
+      && gameData.gameState == GameState.WAIT_ENOUGHT_PLAYERS)
     {
       var delay = 2000;
     	setTimeout(function() {
@@ -96,20 +103,23 @@ io.on('connection', function (socket) {
       // echo globally that this client has left
       socket.broadcast.emit('user left', {
         username: socket.username,
-        numUsers: gameData.numUsers
+        numUsers: gameData.numUsers,
+        iduserleft: socket.player.id
       });
       console.log('user left: %s, numUsers: %d', socket.username, gameData.numUsers);
       if(gameData.numUsers == 0){
         gameData.gameState = GameState.WAIT_ENOUGHT_PLAYERS;
+        countQuestion = 0;
         console.log('Room wait for more player');
       }
     }
   });
 
   socket.on('request move',function(data){
-    console.log('request move '+data.x+', '+data.y);
+    console.log('request move '+data.x+', '+data.y+', '+data.z);
     socket.player.x = data.x;
     socket.player.y = data.y;
+    socket.player.z = data.z;
     io.emit('move to',socket.player);
   });
 });
@@ -128,8 +138,11 @@ function randomInt (low, high) {
 }
 
 function checkEnoughPlayer(){
-  if( gameData.numUsers > 1 && gameData.gameState == GameState.WAIT_ENOUGHT_PLAYERS)
+  if(((countQuestion == 0 && gameData.numUsers >= requestPlayerToStart) || countQuestion > 0) 
+      && gameData.gameState == GameState.WAIT_ENOUGHT_PLAYERS)
+  {
     prepareGame();
+  }
 }
 
 function prepareGame(){
@@ -151,7 +164,9 @@ function startGame(){
   gameData.gameState = GameState.PLAYING;
   io.emit('game ready', {
       timeend: (Date.now() + game_time),
-      questionid: randomInt(0, 100)
+      questionid: randomInt(0, 100),
+      countquestion: (countQuestion + 1),
+      maxquestion: maxQuestion
     });
   setTimeout(function() {
       if(gameData.gameState == GameState.PLAYING)
@@ -162,6 +177,9 @@ function startGame(){
 function endGame(){
   console.log('end game');
   gameData.gameState = GameState.WAIT_ENOUGHT_PLAYERS;
+  countQuestion++;
+  if(countQuestion >= maxQuestion)
+    countQuestion = 0;
   io.emit('game end', {
       result: "ABC"
     });
